9819e99f30fa826154d5acf04919a65e37f8dc8e
Jon Shamir <jon.shamir@gmail.com>
2024-11-04 18:09:06 +0200

    Simplify

--- app/demos/02_viz/utils.ts
+++ app/demos/02_viz/utils.ts
@@ -1,3 +1,30 @@
+interface RawDataRow {
+  code: string;
+  parent: string;
+  func_cls_title_1: string[];
+  func_cls_title_2: string[];
+  net_allocated: number;
+}
+
+const dataQuery = encodeURIComponent(
+  "SELECT code, parent, func_cls_title_1, func_cls_title_2, net_allocated FROM raw_budget WHERE code LIKE 'C8%' AND length(code)=4 AND year=2023"
+);
+const dataEndpoint = `https://next.obudget.org/api/query?query=${dataQuery}`;
+
+export async function getDataForChart() {
+  const res = await fetch(dataEndpoint);
+  const rawData = await res.json();
+
+  const data = rawData.rows.map((item: RawDataRow) => {
+    return {
+      name: item.func_cls_title_2[0],
+      code: item.code,
+      amount: item.net_allocated,
+    };
+  });
+  return data;
+}
+
 export function formatShekels(labelValue: number): string {
   // Nine Zeroes for Billions
   return Math.abs(Number(labelValue)) >= 1.0e9
@@ -9,38 +36,4 @@ export function formatShekels(labelValue: number): string {
     Math.abs(Number(labelValue)) >= 1.0e3
     ? (Math.abs(Number(labelValue)) / 1.0e3).toFixed(1) + "K ₪"
     : Math.abs(Number(labelValue)) + " ₪";
-}
-
-interface RawDataRow {
-  code: string;
-  parent: string;
-  func_cls_title_1: string[];
-  func_cls_title_2: string[];
-  net_allocated: number;
-}
-
-interface RawData {
-  rows: RawDataRow[];
-}
-
-const dataQuery = encodeURIComponent(
-  "SELECT code, parent, func_cls_title_1, func_cls_title_2, net_allocated FROM raw_budget WHERE code LIKE 'C8%' AND length(code)=4 AND year=2023"
-);
-const dataEndpoint = `https://next.obudget.org/api/query?query=${dataQuery}`;
-
-async function getRawData(): Promise<RawData> {
-  const res = await fetch(dataEndpoint);
-  return res.json();
-}
-
-export async function getDataForChart() {
-  const rawData = await getRawData();
-  const data = rawData.rows.map((item: RawDataRow) => {
-    return {
-      name: item.func_cls_title_2[0],
-      code: item.code,
-      amount: item.net_allocated,
-    };
-  });
-  return data;
 }
